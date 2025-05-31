bd863a84293591d4d93aef8c409dfbf4d2aff13e
GGUM <xxoyya24@soongsil.ac.kr>
2025-06-01 04:30:00 +0900

    [FIX] list 오류 해결 중...
    [FIX] list 오류 해결 중...

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,5 +1,5 @@
 const mongoclient = require('mongodb').MongoClient;
-const ObjId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 const url = 'mongodb+srv://xxoyya:1234@cluster0.7clwbsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const express = require('express');
@@ -10,9 +10,6 @@ mongoclient.connect(url)
     .then(client => {
         console.log('몽고DB 접속 성공');
         mydb = client.db('myboard');
-        mydb.collection('post').find().toArray().then(result =>{
-            console.log(result);
-        })
     })
 
     //MYSQL + Node.js 접속 코드
@@ -26,11 +23,10 @@ mongoclient.connect(url)
 
     conn.connect();
 
-    app.set('view engine', 'ejs');
-
     // body-parser 라이브러리 추가
     const bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded({ extended: true }));
+    const db = require('node-mysql/lib/db');app.use(bodyParser.urlencoded({ extended: true }));
+    app.set('view engine', 'ejs');
 
     app.listen(8080, function(){
         console.log("포트 8080으로 서버 대기중 ...")
@@ -45,8 +41,6 @@ mongoclient.connect(url)
             console.log(result);
             res.render('list.ejs', {data: result});
         });
-        res.sendFile(__dirname + '/list.html');
-        res.render('list.ejs');
     });
 
     // '/enter' 요청에 대한 처리 루틴
@@ -56,8 +50,8 @@ mongoclient.connect(url)
 
     app.post('/delete', function(req, res){
         console.log(req.body._id);
-        req.body._id = new ObjId(req.body._id);
-        mydb.collection('post').deleteOne(req.body)
+        req.body._id = new ObjectId(req.body._id);
+    mydb.collection('post').deleteOne(req.body)
         .then(result =>{
             console.log('삭제완료');
             res.status(200).send();
