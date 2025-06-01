34c2cb2b89dcc714a439cd6b53bd51352867a887
GGUM <xxoyya24@soongsil.ac.kr>
2025-06-01 21:40:00 +0900

    [FIX] list 무한로딩 문제 해결
    [FIX] list 무한로딩 문제 해결
    
    #1

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,85 +1,80 @@
-const mongoclient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
-const url = 'mongodb+srv://xxoyya:1234@cluster0.7clwbsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
 const express = require('express');
 const app = express();
+
+const { MongoClient, ObjectId } = require('mongodb');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const url = 'mongodb+srv://xxoyya:1234@cluster0.7clwbsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// MySQL 연결
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'myboard',
+});
+conn.connect();
+
 let mydb;
 
-mongoclient.connect(url)
-    .then(client => {
-        console.log('몽고DB 접속 성공');
-        mydb = client.db('myboard');
-    })
+// MongoDB 연결 + 서버 및 라우터 등록
+MongoClient.connect(url)
+  .then(client => {
+    console.log('몽고DB 접속 성공');
+    mydb = client.db('myboard');
 
-    //MYSQL + Node.js 접속 코드
-    var mysql = require('mysql');
-    var conn = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'myboard',
+    // 📍 라우트 등록은 연결 이후에!
+    app.get('/list', async (req, res) => {
+      try {
+        const result = await mydb.collection('post').find().toArray();
+        console.log(result);
+        res.render('list.ejs', { data: result });
+      } catch (err) {
+        console.error('❌ /list DB 에러:', err);
+        res.status(500).send('DB 오류');
+      }
     });
 
-    conn.connect();
-
-    // body-parser 라이브러리 추가
-    const bodyParser = require('body-parser');
-    const db = require('node-mysql/lib/db');app.use(bodyParser.urlencoded({ extended: true }));
-    app.set('view engine', 'ejs');
-
-    app.listen(8080, function(){
-        console.log("포트 8080으로 서버 대기중 ...")
+    app.get('/enter', (req, res) => {
+      res.render('enter.ejs');
     });
 
-    app.get('/list', function(req, res){
-        // conn.query('SELECT * FROM post', function (err, rows, fields) {
-        //     if (err) throw err;
-        //     console.log(rows);
-        // });
-        mydb.collection('post').find().toArray(function(err, result){
-            console.log(result);
-            res.render('list.ejs', {data: result});
+    app.post('/save', async (req, res) => {
+      try {
+        console.log(req.body.title, req.body.content, req.body.someDate);
+        await mydb.collection('post').insertOne({
+          title: req.body.title,
+          content: req.body.content,
+          date: req.body.someDate,
         });
+        console.log('데이터 추가 성공');
+        res.redirect('/list');
+      } catch (err) {
+        console.error('❌ /save 에러:', err);
+        res.status(500).send('저장 중 오류 발생');
+      }
     });
 
-    // '/enter' 요청에 대한 처리 루틴
-    app.get('/enter', function(req, res){
-        res.render('enter.ejs');
+    app.post('/delete', async (req, res) => {
+      try {
+        const id = new ObjectId(req.body._id);
+        await mydb.collection('post').deleteOne({ _id: id });
+        console.log('삭제 완료');
+        res.sendStatus(200);
+      } catch (err) {
+        console.error('❌ /delete 에러:', err);
+        res.sendStatus(500);
+      }
     });
 
-    app.post('/delete', function(req, res){
-        console.log(req.body._id);
-        req.body._id = new ObjectId(req.body._id);
-    mydb.collection('post').deleteOne(req.body)
-        .then(result =>{
-            console.log('삭제완료');
-            res.status(200).send();
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send();
-        });
+    app.listen(8080, () => {
+      console.log("포트 8080으로 서버 대기중 ...");
     });
-
-    // '/save' 요청에 대한 post 처리 루틴
-    app.post('/save', function(req, res){
-        console.log(req.body.title);
-        console.log(req.body.content);
-        console.log(req.body.someDate);
-        // 몽고 DB에 데이터 저장하기
-        mydb.collection('post').insertOne(
-            {title: req.body.title, content: req.body.content, date: req.body.someDate},
-            ).then(result => {
-                console.log(result);
-                console.log('데이터 추가 성공');
-            });
-        // MYSQL에 데이터 저장하기
-        // let sql = "insert into post (title, content, created) values (?, ?, NOW())";
-        // let params = [req.body.title, req.body.content];
-        // conn.query(sql, params, function(err, result){
-        //     if(err) throw err;
-        //     console.log('데이터 추가 성공');
-        // });
-        res.send('데이터 추가 성공');
-    });
+  })
+  .catch(err => {
+    console.error('❌ MongoDB 연결 실패:', err);
+  });
